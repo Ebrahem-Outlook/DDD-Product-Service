6972be78b7164359aba88a3e60de0e0c863e4b46
Ebrahem-Outlook <EbrahemElbetar1@outlook.com>
2024-07-15 15:46:41 +0300

    V_17

---
+++ Src/Products.Domain/Core/BaseType/Error.cs
@@ -0,0 +1,23 @@
+﻿
+namespace Products.Domain.Core.BaseType;
+
+public sealed class Error : ValueObject
+{
+    public Error(string code, string message)
+    {
+        Code = code;
+        Message = message;
+    }
+
+    public string Code { get; }
+
+    public string Message { get; }
+
+    public static Error None => new Error(string.Empty, string .Empty); 
+
+    protected override IEnumerable<object> GetEqualityComponents()
+    {
+        yield return Code;
+        yield return Message;
+    }
+}
---
+++ Src/Products.Domain/Core/BaseType/Result/Result.cs
@@ -0,0 +1,20 @@
+﻿namespace Products.Domain.Core.BaseType.Result;
+
+public class Result
+{
+    private Result(bool isSuccess, Error error)
+    {
+        IsSuccess = isSuccess;
+        Error = error;
+    }
+
+    public bool IsSuccess { get; set; }
+
+    public bool IsFailer => !IsSuccess;
+
+    public Error Error { get; set; }
+
+    public static Result Success() => new Result(true, Error.None);
+
+    public static Result Failer(Error error) => new Result(false, error);
+}
