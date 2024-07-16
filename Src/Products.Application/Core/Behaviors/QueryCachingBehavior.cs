2ab2a52e5f1dc1af48a57df40e9a9c116241a076
Ebrahem-Outlook <EbrahemElbetar1@outlook.com>
2024-07-16 11:49:06 +0300

    V_34

--- Src/Products.Application/Core/Behaviors/QueryCachingBehavior.cs
+++ Src/Products.Application/Core/Behaviors/QueryCachingBehavior.cs
@@ -1,10 +1,11 @@
 ﻿using MediatR;
+using Products.Application.Core.Abstractions.Messaging;
 using Products.Domain.Core.BaseType.Result;
 
 namespace Products.Application.Core.Behaviors;
 
 internal sealed class QueryCachingBehavior<TRequest, TResponse> : IPipelineBehavior<TRequest, TResponse>
-    where TRequest : ICacheQuery
+    where TRequest : IQuery<TResponse>
     where TResponse : Result
 {
     public Task<TResponse> Handle(TRequest request, RequestHandlerDelegate<TResponse> next, CancellationToken cancellationToken)
